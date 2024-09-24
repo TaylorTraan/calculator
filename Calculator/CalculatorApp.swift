8d57ef0fb10f2eb6311f1575fb8be502c4bb3e11
Taylor Tran <taylortraan0709@gmail.com>
2024-09-23 22:33:43 -0700

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Taylor Tran on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
